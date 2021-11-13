ba9cbab9daf111178cbf86c7dbef8e5f6b22a47a
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-13 12:12:44 +0530

    [Gyanendra] Add: A Program to Change Temprature

--- NUnitProgram/NUnitProgram/Program.cs
+++ NUnitProgram/NUnitProgram/Program.cs
@@ -9,7 +9,7 @@ namespace NUnitProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter your Choice Number to Execute the NUnit Program Press- 1-Vending Machine, 2-Day of Week, 3-Temprature Conversion, 4-Monthly Payment, 5-Square Root, 6-To Binery, 7-Swap Binery, 8-Exit");
+                Console.WriteLine("Enter your Choice Number to Execute the NUnit Program Press- 1-Vending Machine, 2-Temprature Conversion, 3-Day of Week, 4-Monthly Payment, 5-Square Root, 6-To Binery, 7-Swap Binery, 8-Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -20,6 +20,12 @@ namespace NUnitProgram
                         vm.Calculate(amount);
                         break;
                     case 2:
+                        Console.WriteLine("Enter the Temp. to change");
+                        int temp = Convert.ToInt32(Console.ReadLine());
+                        TempratureConversion tc = new TempratureConversion();
+                        tc.Calculate(temp);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
---
+++ NUnitProgram/NUnitProgram/TempratureConversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitProgram
+{
+    class TempratureConversion
+    { //This will Return Cel. to Fah.
+        public void CeltoFeh(float temp)
+        {
+            float value = ((temp * 9.0f / 5.0f) + 32.0f);
+            Console.WriteLine("The Changed Value is:" + value);
+        }
+        //This will Return Cel. to Fah.
+        public void FehToCel(float temp)
+        {
+            float value = (temp - 32.0f) * 5.0f / 9.0f;
+            Console.WriteLine("The Changed Value is:" + value);
+        }
+        public void Calculate(float temp) 
+        {
+            Console.WriteLine("Press 1 to convert Cel. to Feh. or Press 2 to convert Feh. to Cel.");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+                case 1:
+                    TempratureConversion tc = new TempratureConversion();
+                    tc.CeltoFeh(temp);
+                    break;
+                case 2:
+                    TempratureConversion tc1 = new TempratureConversion();
+                    tc1.FehToCel(temp);
+                    break;
+            }
+      }
+    }
+}
